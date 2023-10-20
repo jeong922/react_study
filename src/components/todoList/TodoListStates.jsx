286c7d306dfd216a2a837f26b524877e8d565f0a
jeong922 <hwajeong922@naver.com>
2023-10-20 14:35:47 +0900

    todoList - progress bar

--- src/components/todoList/TodoListStates.jsx
+++ src/components/todoList/TodoListStates.jsx
@@ -8,12 +8,25 @@ export default function TodoListStates() {
 
   const formattedPercentCompleted = Math.round(percentCompleted * 100);
 
+  console.log(totalCompletedNum / totalNum);
+
   return (
     <ul>
       <li>Total items: {totalNum}</li>
       <li>Items completed: {totalCompletedNum}</li>
       <li>Items not completed: {totalUncompletedNum}</li>
-      <li>Percent completed: {formattedPercentCompleted}</li>
+      <li>
+        <span>Percent completed</span>
+        <div className='relative w-48 h-6 bg-rose-100'>
+          <div
+            style={{ width: `${(totalCompletedNum / totalNum) * 100}%` }}
+            className={`bg-rose-400 h-full transition-all duration-300 ease-in-out`}
+          ></div>
+          <p className='absolute top-0 w-full text-center'>
+            {formattedPercentCompleted}%
+          </p>
+        </div>
+      </li>
     </ul>
   );
 }
